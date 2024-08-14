594f90ad0d6a196a69d82878c927799b482e9373
R001h <rachidortega@fwdcostarica.com>
2024-08-14 11:34:48 -0600

    commit

---
+++ src/ejercicio3.js
@@ -0,0 +1,13 @@
+// Inserte el código aquí
+
+class Contador {
+    // Inserte el código aquí
+   valor = 0
+  siguiente (){
+  return this.valor++
+  }
+  
+  }
+
+// NO MODIFICAR
+export { clonarArreglo, clonarObjeto, sumar, saludar };
